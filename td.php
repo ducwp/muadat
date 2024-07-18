f9357d5d8daf949a1de841e53d1681dde950e58e
Đức Nguyễn <ducnv119@gmail.com>
2024-07-18 13:50:29 +0700

    Update td.php

--- td.php
+++ td.php
@@ -5,12 +5,30 @@ $input = '';
 $output = '';
 
 if (isset($_POST['btnSub1'])) {
-  $lines = explode("\n", $_POST['input']);
-  $lines = array_map(function ($item) {
-    return str_replace(",", '.', trim($item));
-  }, $lines);
+  if ($_POST['style'] == '1') {
+    $lines = explode("\n", $_POST['input']);
+    $lines = array_map(function ($item) {
+      return str_replace(",", '.', trim($item));
+    }, $lines);
+
+    $pairs = array_chunk($lines, 2);
+    
+  } else {
+    $parts =  explode('-', $_POST['input']);
+    $line_Xs = explode("\n", $parts[0]);
+    $line_Xs = array_map(function ($item) {
+      return str_replace(",", '.', trim($item));
+    }, $line_Xs);
+    $line_Ys = explode("\n", $parts[1]);
+    $line_Ys = array_map(function ($item) {
+      return str_replace(",", '.', trim($item));
+    }, $line_Ys);
+
+    $pairs =  array_combine($line_Xs, $line_Ys);
+  }
+
   $assoc = array();
-  foreach (array_chunk($lines, 2) as $pair) {
+  foreach ($pairs as $pair) {
     list($key, $value) = $pair;
     $assoc[$key] = $value;
     $output .= trim($key . ', ' . $value) . "\n";
@@ -43,8 +61,6 @@ if (isset($_POST['btnSub2'])) {
   $input = join("\n", $new_input);
 }
 
-
-
 $title = "Toạ độ";
 ?>
 <?php include 'inc/header.php' ?>
@@ -54,6 +70,11 @@ $title = "Toạ độ";
     <div class="col-lg-6 mb-3 mb-lg-0">
       <label for="" class="form-label">Input</label>
       <textarea class="form-control mb-3" name="input" rows="10" cols="80"><?php echo $input; ?></textarea>
+      <p>
+        <label><input type="radio" name="style" value="1" checked> Style #1</label>
+        <label><input type="radio" name="style" value="2"> Style #2</label>
+      </p>
+
       <button name="btnSub1" type="submit" class="btn btn-secondary">Submit #1</button>
     </div>
     <div class="col-lg-6">
